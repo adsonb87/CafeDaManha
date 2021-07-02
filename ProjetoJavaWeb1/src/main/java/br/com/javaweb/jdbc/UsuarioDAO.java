a2f8f49905a3c4486038e548ad99068f9a77c9f7
Your Name <adsonb87@gmail.com>
2021-07-02 20:50:07 -0300

    Finalizado o crud do usuario

--- ProjetoJavaWeb1/src/main/java/br/com/javaweb/jdbc/UsuarioDAO.java
+++ ProjetoJavaWeb1/src/main/java/br/com/javaweb/jdbc/UsuarioDAO.java
@@ -2,7 +2,10 @@ package br.com.javaweb.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.javaweb.entidades.Usuario;
 
@@ -31,6 +34,125 @@ public class UsuarioDAO {
 			
 			e.printStackTrace();
 		}
-		
 	}
+	
+	public void alterarUsuario (Usuario usuario) {
+		//SQL 
+		String sql = "UPDATE USUARIO SET nome = ?, login = ?, senha = ? WHERE ID = ?";
+		
+		//Constroi prStm com o SQL
+		try {
+			PreparedStatement prStm = con.prepareStatement(sql);
+			
+			//Adiciona os valores nas variaveis da SQL
+			prStm.setString(1, usuario.getNome());
+			prStm.setString(2, usuario.getLogin());
+			prStm.setString(3, usuario.getSenha());
+			prStm.setInt(4, usuario.getId());
+			
+			prStm.execute();
+			prStm.close();
+			
+			System.out.println("Alterado com sucesso");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void excluirUsuario (Usuario usuario) {
+		//SQL 
+		String sql = "DELETE FROM USUARIO WHERE ID = ?";
+		
+		//Constroi prStm com o SQL
+		try {
+			PreparedStatement prStm = con.prepareStatement(sql);
+			
+			//Adiciona os valores nas variaveis da SQL
+			prStm.setInt(1, usuario.getId());
+			
+			prStm.execute();
+			prStm.close();
+			
+			System.out.println("Excluído com sucesso");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Usuario> listarUsuario () {
+		//SQL 
+		String sql = "SELECT * FROM USUARIO";
+		
+		ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		
+		
+		//Constroi prStm com o SQL
+		try {
+			PreparedStatement prStm = con.prepareStatement(sql);
+			
+			//ResultSet recebe a query retornada da pesquisa do banco
+			ResultSet rs = prStm.executeQuery();
+			
+			while(rs.next()) {
+				
+				Usuario usuario = new Usuario();
+				
+				usuario.setId(rs.getInt(1));
+				usuario.setNome(rs.getString(2));
+				usuario.setLogin(rs.getString(3));
+				usuario.setSenha(rs.getString(4));
+				
+				listaUsuarios.add(usuario);
+				
+			}
+			
+			rs.close();
+			prStm.close();
+			
+			System.out.println("Listado com sucesso");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return listaUsuarios;
+	}
+	
+	public Usuario buscarPorIdUsuario (Integer id) {
+		//SQL 
+		String sql = "SELECT * FROM USUARIO WHERE ID = ?";	
+
+		Usuario usuario = null;
+		
+		//Constroi prStm com o SQL
+		try {
+			PreparedStatement prStm = con.prepareStatement(sql);
+			
+			prStm.setInt(1, id);
+				
+			//ResultSet recebe a query retornada da pesquisa do banco
+			ResultSet rs = prStm.executeQuery();
+			
+			if(rs.next()) {
+				usuario = new Usuario();
+				usuario.setId(rs.getInt(1));
+				usuario.setNome(rs.getString(2));
+				usuario.setLogin(rs.getString(3));
+				usuario.setSenha(rs.getString(4));
+			}
+			
+			rs.close();
+			prStm.close();
+					
+			System.out.println("Localizado com sucesso");
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+				
+			return usuario;
+	}
+	
 }
--- ProjetoJavaWeb1/src/main/java/br/com/javaweb/teste/TesteUsuarioDAO.java
+++ ProjetoJavaWeb1/src/main/java/br/com/javaweb/teste/TesteUsuarioDAO.java
@@ -1,5 +1,8 @@
 package br.com.javaweb.teste;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.javaweb.entidades.Usuario;
 import br.com.javaweb.jdbc.UsuarioDAO;
 
@@ -7,12 +10,62 @@ public class TesteUsuarioDAO {
 
 	public static void main(String[] args) {
 		
+		testeBuscarPorId();
+
+	}
+
+	private static void testeCadastrar() {
 		Usuario usuario = new Usuario("Adson Souza", "Asouza", "123");
 		
 		UsuarioDAO usuarioDao = new UsuarioDAO();
 		
 		usuarioDao.cadastrarUsuario(usuario);
-
 	}
-
+	
+	private static void testeAlterar() {
+		Usuario usuario = new Usuario("Pedro Henrique", "PedroH", "PH123");
+		usuario.setId(9);
+		
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		
+		usuarioDao.alterarUsuario(usuario);
+		
+	}
+	
+	private static void testeExcluir() {
+		Usuario usuario = new Usuario();
+		usuario.setId(9);
+		
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		
+		usuarioDao.excluirUsuario(usuario);
+	}
+	
+	private static void testeListar() {
+		ArrayList<Usuario> listaUsuarios;
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		
+		listaUsuarios = usuarioDAO.listarUsuario();
+		
+		for(Usuario u: listaUsuarios) {
+			System.out.println(u.getId() + " " + u.getNome() + " " + u.getLogin() + " " + u.getSenha());
+		}
+		
+	}
+	
+	private static void testeBuscarPorId () {
+		
+		Integer id = 1;
+		
+		Usuario usuario = new Usuario();
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		
+		usuario = usuarioDAO.buscarPorIdUsuario(id);
+		
+		System.out.println("ID: " + usuario.getId() + " Nome: " + usuario.getNome() 
+							+ " Login: " + usuario.getLogin() + " Senha: " + usuario.getSenha());
+		
+	}
 }
