c1f45cf5f8b76f02d3d1ea2d306e01b2d226701a
Your Name <adsonb87@gmail.com>
2021-07-06 22:17:11 -0300

    Iniciado a construção do controller

--- ProjetoJavaWeb1/src/main/java/br/com/javaweb/controller/UsuarioController.java
+++ ProjetoJavaWeb1/src/main/java/br/com/javaweb/controller/UsuarioController.java
@@ -1,21 +1,25 @@
 package br.com.javaweb.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class UsuarioController
  */
 public class UsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
     public UsuarioController() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -23,15 +27,28 @@ public class UsuarioController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Chamando doGet");
+		
+		String nome = request.getParameter("nome");
+		
+		//System.out.println("nome:" + nome);
+		
+		String empresa = request.getParameter("empresa");
+		
+		//System.out.println("Empresa: " + empresa);
+				
+		PrintWriter saida = response.getWriter();
+		
+		saida.println("nome:" + nome + " Empresa: " + empresa);
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Chamando doPost");
 		doGet(request, response);
 	}
 
